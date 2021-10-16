70c0b722dc3880e11d898b178a1e32c503836911
Naoto Nishizaka <nszk.nao@gmail.com>
2021-10-16 21:44:52 +0900

    remove Creature

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
 const Color = artifacts.require("Color");
-const Creature = artifacts.require("Creature");
 
 module.exports = function (deployer) {
-  const proxyRegistryAddress = "0xE9200A5ec010d833c07293b3f00f27a59395Be0C";
   deployer.deploy(Color);
-  deployer.deploy(Creature, proxyRegistryAddress);
 };
