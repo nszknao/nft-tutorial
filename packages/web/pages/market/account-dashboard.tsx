5255d2c52d062d6d27481d3839cc6cc699380956
Naoto Nishizaka <nszk.nao@gmail.com>
2021-12-10 23:18:57 +0900

    update

---
+++ packages/web/pages/market/account-dashboard.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { AccountDashboard } from "@/web/pages/account-dashboard";
+
+const AccountDashboardPage: NextPage = () => {
+  return <AccountDashboard />;
+};
+
+export default AccountDashboardPage;
--- packages/web/src/pages/account-dashboard.tsx
+++ packages/web/src/pages/account-dashboard.tsx
@@ -1,5 +1,5 @@
 import { VFC } from "react";
 
 export const AccountDashboard: VFC = () => {
-  return null;
+  return <>account dashboard</>;
 };
