f945e7d9f2dee95a43c69e5a0599170788faddfb
Naoto Nishizaka <nszk.nao@gmail.com>
2021-12-18 01:19:42 +0900

    fix

--- packages/web/src/config/wallets.ts
+++ packages/web/src/config/wallets.ts
@@ -1,12 +1,5 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
-import { NetworkConnector } from "@web3-react/network-connector";
 import { ChainId } from "./chain-id";
-import { rpc } from "./rpc";
-
-export const network = new NetworkConnector({
-  defaultChainId: ChainId.RINKEBY,
-  urls: rpc,
-});
 
 const supportedChainIds = Object.values(ChainId) as number[];
 
